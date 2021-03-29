5f24137e61c531d2fd015415a008d3d1a1ee1979
Yi Feng <yif12@uci.edu>
2021-03-29 00:54:51 +0000

    add piecewise linear regressor

--- intervention_data_preprocessing.py
+++ intervention_data_preprocessing.py
@@ -2,6 +2,7 @@ from pandas import DataFrame
 import numpy as np
 from scipy.optimize import curve_fit
 from scipy import log
+import pwlf
 
 # Get intervention data
 def get_intervention_data(no_missing_data: DataFrame) -> DataFrame:
@@ -22,9 +23,17 @@ class Regressor:
     def __init__(self):
         self.regression_func = None
         self.parameter_names: list[str] = None
-        self.clustering_names: list[str] = None
+        self.clustering_parameters: list[str] = None
 
-    def fit_row(self, x, y):
+    def set_clustering_parameters(self, clustering_parameters: list[str]):
+        if clustering_parameters is None:
+            self.clustering_parameters = self.parameter_names.copy()
+            return
+        for param in clustering_parameters:
+            if param in self.parameter_names:
+                self.clustering_parameters.append(param)
+
+    def fit_row(self, x, y): # x,y are np.array
         result = {}
         params_opt, pcov = curve_fit(self.regression_func, x, y)
         result['parameters'] = params_opt
@@ -54,11 +63,62 @@ class Regressor:
         return fit_scores
 
 class LogRegressor(Regressor):
-    def __init__(self, option):
+    def __init__(self, clustering_parameters: list[str] = None):
         Regressor.__init__(self)
         self.regression_func = lambda x, s, b: s * log(x) + b
         self.parameter_names = ['log_slope', 'log_bias']
-        self.clustering_names = self.parameter_names[option]
+        self.clustering_parameters = []
+        self.set_clustering_parameters(clustering_parameters)
+
+class LinearRegressor(Regressor):
+    def __init__(self, clustering_parameters: list[str] = None):
+        Regressor.__init__(self)
+        self.regression_func = lambda x, s, b: s * x + b
+        self.parameter_names = ['linear_slope', 'linear_bias']
+        self.clustering_parameters = []
+        self.set_clustering_parameters(clustering_parameters)
+
+class PiecewiselinRegressor(Regressor):
+    def __init__(self, clustering_parameters: list[str] = None):
+        Regressor.__init__(self)
+        self.parameter_names = ['knot', 'slope1', 'slope2']
+        self.clustering_parameters = []
+        self.set_clustering_parameters(clustering_parameters)
+    
+    # Use a library based on https://jekel.me/piecewise_linear_fit_py/pwlf.html
+    # Before running, pip install pwlf
+    def find_opt_knot(self, x, y) -> int:
+        # Set different knot locations
+        pwlf_each_row = pwlf.PiecewiseLinFit(x, y)
+        r2_all_knots = {}
+        for k in range(2, len(y)):
+            pwlf_each_row.fit_with_breaks_opt([k])
+            r2 = pwlf_each_row.r_squared()
+            r2_all_knots[k] = r2
+        # Find knot location with max r2
+        max_r2 = max(r2_all_knots.values())
+        opt_knot = [key for key in r2_all_knots if r2_all_knots[key] == max_r2]
+        return opt_knot
+
+    def is_unchanged_input(self, y) -> bool:
+        for i in range(0, len(y) - 1):
+            if y[i] != y[i+1]:
+               return False
+        return True
+
+    def fit_row(self, x, y):
+        result = {}
+        if self.is_unchanged_input(y):
+            result['parameters'] = [np.nan] * len(self.parameter_names)
+            result['r2'] = np.nan
+            return result
+        opt_knot = self.find_opt_knot(x, y)
+        pwlf_each_row = pwlf.PiecewiseLinFit(x, y)
+        pwlf_each_row.fit_with_breaks_opt([opt_knot])
+        slopes = pwlf_each_row.calc_slopes()
+        result['parameters'] = np.insert(slopes, 0, opt_knot)
+        result['r2'] = pwlf_each_row.r_squared()
+        return result
 
 '''
 class LinearRegressor(Regressor):
